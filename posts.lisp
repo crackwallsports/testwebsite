f756e66bb62f36576dd77137ef86aa7e3243a9d9
crackwallsports <crackwallsports@gmail.com>
2018-10-11 12:56:41 +0800

    更新News 修改样式 修补错误

--- posts.lisp
+++ posts.lisp
@@ -1,10 +1,11 @@
 ;; (Date Ref Title Intro)
-(("2018 10 10"
+(("2018 10 11"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div () (span (:class "badge badge-success") "每日追踪") (span () "郭文贵"))
-     (div () (span (:class "badge badge-success") "更新") (span () "中共国 供应链 恶意芯片植入 事件"))))
+     (div () ;; (span (:class "badge badge-success") "更新")
+          (span () "中共国 供应链 恶意芯片植入 事件"))))
  ("2017 7 22"
    "/testwebsite/articles/2017/7/web-learn-nodeexpress.html"
    "Web 开发学习笔记 - SbyS: Node.js Express MongoDB"
