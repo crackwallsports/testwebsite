234e0a5181589798ec22376d172d81b8592d9979
crackwallsports <crackwallsports@gmail.com>
2018-11-19 12:34:00 +0800

    U: 更新News信息 20181119

--- posts.lisp
+++ posts.lisp
@@ -1,13 +1,13 @@
 ;; (Date Ref Title Intro)
-(("2018 11 18"
+(("2018 11 19"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div ()
           (span () "郭文贵")
-          (span (:class "badge badge-success") "每日追踪-11.18")
+          (span (:class "badge badge-success") "每日追踪-11.19")
           (pre ()
-               (small () "王健之死 与 海航集团背后的真相 发布会 2018.11.20  倒计时: 2 天")
+               (small () "王健之死 与 海航集团背后的真相 发布会 2018.11.20  倒计时: 1 天")
                (small () "反盗国贼 第一招 第一式 : 隔山杀盗")
                (small () "反CCP联盟")))
      (div ()
