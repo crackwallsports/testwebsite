835f5aaef9f9ef2f26e1499c675c23cdb3413d7e
crackwallsports <crackwallsports@gmail.com>
2019-02-26 23:55:32 +0800

    U: News信息 20190226

--- posts.lisp
+++ posts.lisp
@@ -1,11 +1,11 @@
 ;; (Date Ref Title Intro)
-(("2019 2 25"
+(("2019 2 26"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div ()
           (span () "郭文贵")
-          (span (:class "badge badge-success") "每日追踪-2019.2.25")
+          (span (:class "badge badge-success") "每日追踪-2019.2.26")
           (pre ()
                (small () "法治基金")
                (small () "王健之死")
