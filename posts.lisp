68161fcd209d2246ccd3bdc3151c8a0a50376042
crackwallsports <crackwallsports@gmail.com>
2019-03-31 22:45:33 +0800

    U: News信息 20190331

--- posts.lisp
+++ posts.lisp
@@ -1,11 +1,11 @@
 ;; (Date Ref Title Intro)
-(("2019 3 29"
+(("2019 3 31"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div ()
           (span () "郭文贵")
-          (span (:class "badge badge-success") "每日追踪-2019.3.29")
+          (span (:class "badge badge-success") "每日追踪-2019.3.31")
           (pre ()
                (small () "郭先生 母亲逝世  守丧 🕯")
                (small () "法治基金")
