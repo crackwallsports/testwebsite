ca052432f3ef4a2f80413f6506a85bb0993121f8
crackwallsports <crackwallsports@gmail.com>
2019-07-29 22:19:04 +0800

    U: News信息 20190729

--- articles/News.lisp
+++ articles/News.lisp
@@ -162,7 +162,7 @@ z-index:100;
 
 (news-to-topic
  "郭文贵"  ;; (twb::human-date (get-universal-time))
- "2019.07.27 21:40:50"
+ "2019.07.29 22:18:24"
  (u (:- "信息源"
         (u (:- "郭媒体 : " (@ "https://www.guo.media/milesguo" "@milesguo"))
            (:- "Youtube : " (@ "https://www.youtube.com/channel/UCO3pO3ykAUybrjv3RBbXEHw/featured" "郭文贵"))
--- posts.lisp
+++ posts.lisp
@@ -1,11 +1,11 @@
 ;; (Date Ref Title Intro)
-(("2019 7 27"
+(("2019 7 29"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div ()
           (span () "郭文贵")
-          (span (:class "badge badge-success") "每日追踪-2019.7.27")
+          (span (:class "badge badge-success") "每日追踪-2019.7.29")
           (pre ()
                (small () "法治基金")
                (small () "王健之死")))))
