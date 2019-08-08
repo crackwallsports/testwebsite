4bca6892e7ed9c3d2bb14e4919cb53d1bb8c95c3
crackwallsports <crackwallsports@gmail.com>
2019-08-08 20:15:20 +0800

    U: News信息 20190808

--- articles/News.lisp
+++ articles/News.lisp
@@ -166,7 +166,7 @@ z-index:100;
 
 (news-to-topic
  "郭文贵"   ;; (twb::human-date (get-universal-time))
- "2019.08.06 20:28:50"
+ "2019.08.08 20:15:05"
  (u (:- "信息源"
         (u (:- "郭媒体 : " (@ "https://www.guo.media/milesguo" "@milesguo"))
            (:- "Youtube : " (@ "https://www.youtube.com/channel/UCO3pO3ykAUybrjv3RBbXEHw/featured" "郭文贵"))
@@ -177,7 +177,7 @@ z-index:100;
            (:- '(span (:class "badge badge-secondary") "修改增加: ") "反对以假治国")))
     (:- "蓝金黄 3F美国计划")
     (:- "海航 王健之死")
-    (:- "关注 香港 台湾")
+    (:- "关注 香港 台湾 新疆 西藏")
     (:- "法治基金"
         (u (:- `(cite () "RULE OF LAW FOUNDATION "
                       ,(@ "https://rolfoundation.org")))
--- posts.lisp
+++ posts.lisp
@@ -1,11 +1,11 @@
 ;; (Date Ref Title Intro)
-(("2019 8 6"
+(("2019 8 8"
   "/testwebsite/articles/News.html"
   "News 新鲜事"
   (p ()
      (div ()
           (span () "郭文贵")
-          (span (:class "badge badge-success") "每日追踪-2019.8.6")
+          (span (:class "badge badge-success") "每日追踪-2019.8.8")
           (pre ()
                (small () "法治基金")
                (small () "王健之死")
