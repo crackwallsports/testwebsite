6edc66aebd4717423bdf61462e0b0149ae2c8b38
crackwallsports <crackwallsports@gmail.com>
2019-08-16 22:12:45 +0800

    U: News信息 20190816

--- articles/News.lisp
+++ articles/News.lisp
@@ -166,7 +166,7 @@ z-index:100;
 
 (news-to-topic
  "郭文贵"    ;; (twb::human-date (get-universal-time))
- "2019.08.14 21:48:25"
+ "2019.08.16 22:07:20"
  (u (:- "信息源"
         (u (:- "郭媒体 : " (@ "https://www.guo.media/milesguo" "@milesguo"))
            (:- "Youtube : " (@ "https://www.youtube.com/channel/UCO3pO3ykAUybrjv3RBbXEHw/featured" "郭文贵"))
